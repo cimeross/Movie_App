dfc4d9d7725e98994f6ea6db8973144786d7c614
Milan Suvajac <milansuvajac021@gmail.com>
2023-03-08 01:39:10 +0100

    feat: Add components and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import "./App.scss";
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <div>App</div>;
+	return (
+		<div className="app">
+			<Router>
+				<Header />
+				<div className="container">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/movie/:imdbID" element={<MovieDetail />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</div>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/common/apis/movieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "efdcb9a1";
