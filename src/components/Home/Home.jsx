d7294ebba89bc7a4c5d666503f43e956897448de
Milan Suvajac <milansuvajac021@gmail.com>
2023-03-08 19:49:35 +0100

    Add redux toolkit, styled components and API integration to project

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-	return <div>Home</div>;
+	const movieText = "Lord of The Rings";
+	const dispatch = useDispatch();
+	useEffect(() => {
+		const fetchMovies = async () => {
+			const response = await movieApi
+				.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+				.catch((err) => {
+					console.log("Error :", err);
+				});
+			dispatch(addMovies(response.data));
+		};
+		fetchMovies();
+	}, []);
+	return (
+		<div>
+			<div className="banner-img"></div>
+			<MovieListing />
+		</div>
+	);
 };
 
 export default Home;
